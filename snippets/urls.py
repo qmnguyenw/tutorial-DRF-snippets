17fe0dfffb8046cba6d80183d10754c77046daba
Quang Nguyen <qmnguyenw@gmail.com>
2020-01-08 05:43:36 -0500

    Adding pagination

--- snippets/urls.py
+++ snippets/urls.py
@@ -13,7 +13,7 @@ from django.urls import path, include
 from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
+urlpatterns = format_suffix_patterns([
     path('', views.api_root),
     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
@@ -21,6 +21,6 @@ urlpatterns = [
     path('users/', views.UserList.as_view(), name='user-list'),
     path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
     path('api-auth/', include('rest_framework.urls')),    
-]
+])
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
