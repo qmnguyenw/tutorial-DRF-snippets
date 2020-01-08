4e3fc59d85ccd233e707b29ce72954be021ebade
Quang Nguyen <qmnguyenw@gmail.com>
2020-01-08 05:21:33 -0500

    Hyperlinking our API

--- snippets/urls.py
+++ snippets/urls.py
@@ -19,8 +19,8 @@ urlpatterns = [
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
     path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
     path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-    path('api-auth/', include('rest_framework.urls'), name='user-detail'),    
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('api-auth/', include('rest_framework.urls')),    
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
