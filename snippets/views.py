9fcf71e9ea7a487db6b3b8b1fc5b121374f72a1e
Quang Nguyen <qmnguyenw@gmail.com>
2020-01-07 23:36:38 -0500

    Object level permissions + Authenticating with the API

--- snippets/views.py
+++ snippets/views.py
@@ -188,6 +188,8 @@ from snippets.serializers import UserSerializer
 
 from rest_framework import permissions
 
+from snippets.permissions import IsOwnerOrReadOnly
+
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
@@ -199,7 +201,8 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                            IsOwnerOrReadOnly]
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
