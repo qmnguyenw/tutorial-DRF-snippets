648325071bb4237ff4cc01a21aec6a2fb06cf874
Quang Nguyen <qmnguyenw@gmail.com>
2020-01-09 21:19:42 -0500

    add admin page

--- snippets/models.py
+++ snippets/models.py
@@ -36,3 +36,6 @@ class Snippet(models.Model):
                                 full=True, **options)
         self.highlighted = highlight(self.code, lexer, formatter)
         super(Snippet, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
--- snippets/views.py
+++ snippets/views.py
@@ -181,15 +181,10 @@
 # Step 5:
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import generics
-
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
-
-from rest_framework import permissions, renderers
-
+from rest_framework import permissions, renderers, generics
 from snippets.permissions import IsOwnerOrReadOnly
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
